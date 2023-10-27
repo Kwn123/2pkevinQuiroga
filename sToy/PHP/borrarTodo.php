61ca4d7d59cb3f579801258550ab5594cda8e325
Kwn123 <45769804+Kwn123@users.noreply.github.com>
2023-10-27 16:38:35 -0300

    Delete sToy directory

--- sToy/PHP/borrarTodo.php
+++
@@ -1,9 +0,0 @@
-
-<script>
-    var alerta = confirm('¿Estás seguro de que deseas eliminar a TODOS LOS ALUMNOS?');
-    if (alerta) {
-        window.location = '../ABM/lista.php?eliminar=true>';
-    } else {
-        window.location = '../index.php';
-    }
-</script>
